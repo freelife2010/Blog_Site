a3bb2b41c810f65d0b400769801bc4da1e540907
freelife2010 <snowman190011@gmail.com>
2019-07-19 14:50:51 +1000

    Blog site

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+    def index
+        @articles = Article.paginate(page: params[:page], per_page: 5)
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        # render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        @article.user = User.first
+        if @article.save
+            flash[:success] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:success] = "Article was updated"
+            redirect_to article_path(@article)
+        else
+            flash[:success] = "Article was not updated"
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        flash[:success] = "Article was deleted"
+        redirect_to articles_path
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+end
